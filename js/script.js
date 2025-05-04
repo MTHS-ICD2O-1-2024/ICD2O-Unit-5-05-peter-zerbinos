d2dd940b806057a90efe41d1f01f83d08d6cb165
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 15:35:38 +0000

    added html, js, and css changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculatePay() {
+  const TAX_RATE = 0.18
+  
+  // Input
+  const hoursWorked = document.getElementById("hours").value
+  const hourlyWage = document.getElementById("rate").value
+
+  // Process
+  const taxes = hoursWorked * hourlyWage * TAX_RATE
+  const takeHomeSalary = hoursWorked * hourlyWage - taxes
+
+  // Output
+  document.getElementById("pay").innerHTML =
+    "Your pay will be: $" + takeHomeSalary.toFixed(2)
+  document.getElementById("tax").innerHTML =
+    "The government will take: $" + taxes.toFixed(2)
+}
