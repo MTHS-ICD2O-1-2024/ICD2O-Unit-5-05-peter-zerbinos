8feb34ba096fadd5cb13148246fe6a38d99dd275
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 15:40:34 +0000

    I put my code in the wrong repo

--- js/script.js
+++ js/script.js
@@ -1,25 +1,5 @@
-// Copyright (c) 2025 Peter Zerbinos All rights reserved
+// Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Peter Zerbinos
-// Created on: May 2025
+// Created by: Mr. Coxall
+// Created on: Sep 2020
 // This file contains the JS functions for index.html
-
-"use strict"
-
-function calculatePay() {
-  const TAX_RATE = 0.18
-  
-  // Input
-  const hoursWorked = document.getElementById("hours").value
-  const hourlyWage = document.getElementById("rate").value
-
-  // Process
-  const taxes = hoursWorked * hourlyWage * TAX_RATE
-  const takeHomeSalary = hoursWorked * hourlyWage - taxes
-
-  // Output
-  document.getElementById("pay").innerHTML =
-    "Your pay will be: $" + takeHomeSalary.toFixed(2)
-  document.getElementById("tax").innerHTML =
-    "The government will take: $" + taxes.toFixed(2)
-}
